a203666eec9999995af639d7374079a8f5b7edbe
Naven Prasad <me@naven.digital>
2026-01-20 10:00:00 +0800

    Initial dotfiles commit

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>a", "ggVGy+", { desc = "Yank entire file to clipboard" })
+map("i", "jj", "<Esc>", { silent = true, desc = "jj to escape" })
+
+map("n", "<leader>rr", "<cmd>luafile $MYVIMRC<cr>", { desc = "Reload Neovim config" })
+map("n", "jk", ":", { desc = "Command-line mode" })
